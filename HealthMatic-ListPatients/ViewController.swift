2197d2ff53e94af16003a4ecfca6a4ae9d0191d8
shelalaine <schan211@my.centennialcollege.ca>
2016-10-19 10:56:21 -0400

    Initial Commit

---
+++ HealthMatic-ListPatients/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthMatic-ListPatients
+//
+//  Created by Shelalaine Chan on 2016-10-19.
+//  Copyright © 2016 Team SPY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
